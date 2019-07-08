9238ee18ae0afa61b69fad057010820a1a14f851
David Losas <45861490+Electrobite@users.noreply.github.com>
2019-07-08 16:28:56 +0200

    First Commit

---
+++ challenges/simple-adding.js
@@ -0,0 +1,15 @@
+function SimpleAdding(num) { 
+    
+    let sum = num;
+    
+    for (num ; num > 0 ; num--) {
+        
+        sum = sum + (num-1); 
+        
+    }
+  
+    console.log (sum);
+    
+}
+   
+SimpleAdding(5);
